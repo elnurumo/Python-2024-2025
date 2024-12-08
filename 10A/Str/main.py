a6f8f9c5c876c3fee68205b5214c7e70c1273eb5
elnurumo <elnurqasimov592@gmail.com>
2024-12-08 12:00:40 +0400

    Etrafli setir

--- 10A/Str/main.py
+++ 10A/Str/main.py
@@ -1,61 +1,158 @@
 
 
-print(5, end=' ')
-print(3,end='-')
-print(4)
-print(2)
+# print(5, end=' ')
+# print(3,end='-')
+# print(4)
+# print(2)
 
-a = 5
-b = a
-a = 6
-print(a,b)
+# a = 5
+# b = a
+# a = 6
+# print(a,b)
 
-a = [2,4,6]
-b = a
-a.append(7)
-b = b+[3,4,5] # qrılma nöqtəsi
-print(a,b)
+# a = [2,4,6]
+# b = a
+# a.append(7)
+# b = b+[3,4,5] # qrılma nöqtəsi
+# print(a,b)
 
-a = [2,4,6]
-b = [2,4,6]
-a.append(7)
-print(a,b)
-
-
-n = 5
-n = list(str(n))
-print(n)
-
-def f(x,y):
-    return x+y*x-y
-x = 5
-y = 6
-print(f(6,5))
+# a = [2,4,6]
+# b = [2,4,6]
+# a.append(7)
+# print(a,b)
 
 
-#  method nedi? funksiya nedi?
+# n = 5
+# n = list(str(n))
+# print(n)
 
-a = []
-a
+# def f(x,y):
+#     return x+y*x-y
+# x = 5
+# y = 6
+# print(f(6,5))
+
+
+# #  method nedi? funksiya nedi?
+
+# a = []
+# a
+# print(a)
+
+
+# # join methodu
+# a = ['Toğrul', "Fexri", 'Yusif', 'Valide']
+# x = ' '.join(a)
+# print(x)
+
+
+# # split vs list
+
+# a = 'Salam dunya necesen sagol getdim'
+# # x = list(a) qessab
+# x = a.split('e')
+# print(x)
+
+
+# c = '7'=='4'
+# print(c)
+
+# m = ['A','a','b','B']
+# print(max(m))
+
+# Suallar ------->>>>>>>
+# 70
+
+# a = [2,3,4,5]
+# b =  [2,3,4,5]
+# a.append(23)
+# print(a,b)
+
+# print(2,3,4,5, sep='@')
+
+
+# print(1,end=' ')
+# print(2,end=' ')
+# print(3,end=' ')
+# print(4,end=' ')
+# print()
+# print(5)
+
+# # 57
+
+# a = ['Toğrul','Fexri', 'Revane', 'Yusif']
+# b =[]
+# for i in a:
+#     b.append(len(i))
+# print(max(b))
+
+# # 59
+
+# n = int(input()) # 345
+# k = len(str(n))
+# if k > 2:
+#     n = str(n)
+#     n = list(n)
+#     n.sort()
+#     n.reverse()
+#     n = ''.join(n)
+#     n = int(n)
+#     print(n)
+
+# # 63
+
+
+# b = []
+# for i in range(1,8):
+#     n = int(input(f'{i}. gün: '))
+#     b.append(n)
+# print(b)
+
+# # 64
+
+# n = int(input())
+# if n>0:
+#     n = list(str(n))
+#     n.reverse()
+#     n = ''.join(n)
+#     n = int(n)
+#     print(n)
+# else:
+#     print('Eded musbet deyil')
+
+
+
+## 66
+
+# n = input()
+# n = n.replace('a','*')
+# n = n.replace('b','a')
+# n = n.replace('*','b')
+# print(n)
+
+
+# # 70
+
+# n = input() # ssbbbsssbc
+# s = 0
+# b = []
+# for i in n:
+#     if i == 's':
+#         s = s + 1
+#     else:
+#         b.append(s)
+#         s = 0
+# print(max(b))
+    
+
+
+## 72
+
+a = [2314,342,1206,4321,57,78965]
+for i in range(0,len(a)):
+    b = str(a[i])
+    ma = int(max(b))
+    mi = int(min(b))
+    c = ma*ma + mi*mi
+    a[i] = c 
 print(a)
-
-
-# join methodu
-a = ['Toğrul', "Fexri", 'Yusif', 'Valide']
-x = ' '.join(a)
-print(x)
-
-
-# split vs list
-
-a = 'Salam dunya necesen sagol getdim'
-# x = list(a) qessab
-x = a.split('e')
-print(x)
-
-
-c = '7'=='4'
-print(c)
-
-m = ['A','a','b','B']
-print(max(m))
--- 10B/Str/main.py
+++ 10B/Str/main.py
@@ -1,46 +1,139 @@
-# 35,36,38,41,37
+# # 35,36,38,41,37
 
-# 16
+# # 16
 
-a = [1,2,3,2,4,1,5,6,5]
-b = []
-for i in a :
-    if a.count(i) > 1:
-        if b.count(i) == 0:
-            s = a.count(i)
-            b.append(s)
-print(b)
+# a = [1,2,3,2,4,1,5,6,5]
+# b = []
+# for i in a :
+#     if a.count(i) > 1:
+#         if b.count(i) == 0:
+#             s = a.count(i)
+#             b.append(s)
+# print(b)
 
-# 19
+# # 19
 
 
-n = 5
-n = list(str(n))
-print(n)
+# n = 5
+# n = list(str(n))
+# print(n)
 
-a = ['Elnur','Amin','Fizuli','Məryəm','Yusif']
-x = ' '.join(a)
-print(x)
+# a = ['Elnur','Amin','Fizuli','Məryəm','Yusif']
+# x = ' '.join(a)
+# print(x)
 
-a = 'Meryem Fizuli Amin Yusif'
-x = a.split()
-print(x)
+# a = 'Meryem Fizuli Amin Yusif'
+# x = a.split()
+# print(x)
 
-# split() vs list()
+# # split() vs list()
 
-a = 'Meryem Fizuli Amin Yusif'
-b = list(a)
-x = a.split()
-print(x,b)
+# a = 'Meryem Fizuli Amin Yusif'
+# b = list(a)
+# x = a.split()
+# print(x,b)
  
 
-print(5,end=' ')
-print(4)
-print(3)
-print(6)
+# print(5,end=' ')
+# print(4)
+# print(3)
+# print(6)
 
 
 
+# # 9
 
+# a = [2,4,6]
+# b = [2,4,6]
+# print(a==b)
+
+
+# s = 1
+# for i in range(1,6):
+#     s *= i
+# print(s)
+
+# a = 'Salam'
+
+# f = a.index('s')
+# print(f)
+
+# range(6)
+
+# a = [15,4,3,16]
+# a.insert(3,30)
+# print(a)
+
+# a = 'salazZ'
+# m = max(a)
+# print(m)
+
+# if '9'>'2':
+#     print('salam')
+
+
+# Suallar
+
+# 63,69,66,73,72,74,68
+
+# # 57
+
+# a = ['salam','Azerbaycan','baki']
+# b = []
+# for i in a:
+#     l = len(i)
+#     b.append(l)
+# print(max(b))
+
+# # 58
+# n = input().split()
+# sN = 1 
+# for i in n:
+#     s = i.count('w')
+#     print('daxil edilmiş cümlenin', 
+#     sN, "saylı sözündə",s
+#     ,'sayda w simvolu mövcuddur.')
+#     sN = sN + 1
+
+
+# # 59
+
+# n = int(input()) #213 -->>123-->>321
+# n = str(n)
+# k = len(n)
+# if k>2:
+#     n = list(n)
+#     n.sort()
+#     n.reverse() # ['3','2','1']
+#     n = ''.join(n)
+#     n = int(n)
+#     print(n)
+
+
+# # 61
+
+# n = input() # Sd234rdf12
+# reqemler = '0123456789'
+# b = []
+# for i in n:
+#     if reqemler.find(i) == -1:
+#         b.append(i)
+# n = ''.join(b)
+# print(n)
+
+
+# # 62
+
+# n = input()
+# R = '0123456789'
+# b = []
+# for i in R:
+#     if n.count(i) == 0:
+#         b.append(i)
+# print(b,len(b))
+
+
+
+# 71
 
 
